f65c9ce02e140cd109a855ec14820d24dcf66a86
Kazure Zheng <able7252@outlook.com>
2025-05-22 08:20:33 +0800

    add aweshell to enhanced the shell

--- init.el
+++ init.el
@@ -63,7 +63,9 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/org-re-reveal/")
 (require 'org-re-reveal)
 
-(define-key global-map "\C-ce" 'eshell)
+(add-to-list 'load-path "~/.emacs.d/site-lisp/aweshell/")
+(require 'aweshell)
+(define-key global-map "\C-ce" 'aweshell-toggle)
 
 (add-to-list 'load-path "~/.emacs.d/site-lisp/ivy/")
 (require 'ivy)
